11b414e42211fcfd75b6770b91a6f1041a26c7af
Abel Orode <48480130+AbelOrode@users.noreply.github.com>
2021-07-15 11:42:30 +0100

    created product module

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\StaffController;
 use App\Http\Controllers\Api\SupplierController;
 use Illuminate\Http\Request;
@@ -29,3 +29,4 @@ Route::group([
 
 Route::ApiResource('/staff', StaffController::class);
 Route::ApiResource('/supplier', SupplierController::class);
+Route::ApiResource('/product', ProductController::class);
